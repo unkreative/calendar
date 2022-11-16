a4bdc7f1bb8952f3be2bcb30cf799b0d7c8e32f5
unkreative <78596808+unkreative@users.noreply.github.com>
2022-11-16 12:56:59 +0100

    install composer and symfony

---
+++ bootstrap.php
@@ -0,0 +1,34 @@
+<?php
+// bootstrap.php
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+require_once "vendor/autoload.php";
+
+// Create a simple "default" Doctrine ORM configuration for Attributes
+$config = ORMSetup::createAttributeMetadataConfiguration(
+    paths: array(__DIR__."/src"),
+    isDevMode: true,
+);
+// or if you prefer annotation, YAML or XML
+// $config = ORMSetup::createAnnotationMetadataConfiguration(
+//    paths: array(__DIR__."/src"),
+//    isDevMode: true,
+// );
+// $config = ORMSetup::createXMLMetadataConfiguration(
+//    paths: array(__DIR__."/config/xml"),
+//    isDevMode: true,
+//);
+// $config = ORMSetup::createYAMLMetadataConfiguration(
+//    paths: array(__DIR__."/config/yaml"),
+//    isDevMode: true,
+// );
+
+// database configuration parameters
+$conn = array(
+    'driver' => 'pdo_sqlite',
+    'path' => __DIR__ . '/db.sqlite',
+);
+
+// obtaining the entity manager
+$entityManager = EntityManager::create($conn, $config);
