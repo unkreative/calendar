b610ef1eed95d6195860a1285b409c191662bf0c
unkreative <78596808+unkreative@users.noreply.github.com>
2022-11-23 10:15:38 +0100

    make demo

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,5 +1,5 @@
 console.log("aa")
-fetch("navbar.html")
+fetch("/pages/navbar.html")
     .then(response => response.text())
     // .then(text => console.log(text))
     .then(text => nav.insertAdjacentHTML("afterbegin", text))
